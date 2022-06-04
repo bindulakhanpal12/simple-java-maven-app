971c9a61848e0195e5d78327035068d351b9a609
bindulakhanpal12 <bindu.lakhanpal@gmail.com>
2022-06-04 12:50:03 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello  Dear Bindu João Pires!";
+    private final String message = "Hello  Dear Binduuu João Pires!";
 
     public App() {}
 
