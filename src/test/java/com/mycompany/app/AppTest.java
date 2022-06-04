3262e223ed96a36b538c5dd4e6da87d1a1e4ec98
bindulakhanpal12 <bindu.lakhanpal@gmail.com>
2022-06-04 13:02:09 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello João Pires!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello  Dear Binduuu João Pires!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
